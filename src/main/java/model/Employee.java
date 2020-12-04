f524e7b584da4eafd64c2f1f8c7f5629297862f7
rahul.r.mandre <rahulmandre74@gmail.com>
2020-12-04 14:55:26 +0530

    added spring dependency

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Employee {
+    int eId;
+    String eName;
+    int eSalary;
+
+    public Employee(int eId, String eName, int eSalary) {
+        this.eId = eId;
+        this.eName = eName;
+        this.eSalary = eSalary;
+    }
+
+    public Employee() {
+
+    }
+
+    public int getEId() {
+        return eId;
+    }
+
+    public void setEId(int eiId) {
+        this.eId = eiId;
+    }
+
+    public String geteName() {
+        return eName;
+    }
+
+    public void seteName(String eName) {
+        this.eName = eName;
+    }
+
+    public int geteSalary() {
+        return eSalary;
+    }
+
+    public void seteSalary(int eSalary) {
+        this.eSalary = eSalary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "eiId=" + eId +
+                ", eName='" + eName + '\'' +
+                ", eSalary=" + eSalary +
+                '}';
+    }
+}
